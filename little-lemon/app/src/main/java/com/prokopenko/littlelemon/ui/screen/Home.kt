20ecc5debe903542442de4fc7531e106ec8d64eb
Pavlo <stpasha6@gmail.com>
2024-04-09 18:05:33 +0300

    Create home header

--- little-lemon/app/src/main/java/com/prokopenko/littlelemon/ui/screen/Home.kt
+++ little-lemon/app/src/main/java/com/prokopenko/littlelemon/ui/screen/Home.kt
@@ -1,29 +1,47 @@
 package com.prokopenko.littlelemon.ui.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.prokopenko.littlelemon.R
 import com.prokopenko.littlelemon.ui.navigation.Destinations
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+    Row (
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "Home",
+        Image(
+            painter = painterResource(id = R.drawable.logo) ,
+            contentDescription = "Little lemon logo",
             modifier = Modifier
+                .padding(top = 16.dp, bottom = 16.dp, start = 50.dp)
+                .height(50.dp)
+                .weight(1F, true))
+
+        Image(
+            painter = painterResource(id = R.drawable.profile) ,
+            contentDescription = "profile",
+            modifier = Modifier
+                .padding(vertical = 16.dp, horizontal = 16.dp)
+                .size(50.dp)
                 .clickable {
                     navController.navigate(Destinations.Profile.getRoute())
-                })
+                }
+        )
     }
 }
 
